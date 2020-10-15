b8233a11210884a8057215980ff6884295e50a01
NoSequel <nosequel@protonmail.com>
2020-10-15 20:40:08 +0000

    add patch for paginated menus

--- src/main/java/io/github/nosequel/katakuna/menu/paginated/PaginatedMenu.java
+++ src/main/java/io/github/nosequel/katakuna/menu/paginated/PaginatedMenu.java
@@ -20,6 +20,12 @@ public class PaginatedMenu extends Menu {
 
     private int page;
 
+    private final List<Button> paginationButtons = new ArrayList<>(Arrays.asList(
+            new PaginationButton(0, PaginationType.PREVIOUS, this),
+            new PaginationButton(8, PaginationType.NEXT, this))
+    );
+
+
     /**
      * Constructor for creating a new PaginatedMenu {@link Menu}
      *
@@ -32,11 +38,7 @@ public class PaginatedMenu extends Menu {
 
     @Override
     public void updateMenu() {
-        final List<Button> buttons = new ArrayList<>(Arrays.asList(
-                new PaginationButton(0, PaginationType.PREVIOUS, this),
-                new PaginationButton(8, PaginationType.NEXT, this))
-        );
-
+        final List<Button> buttons = new ArrayList<>(paginationButtons);
         buttons.addAll(this.getButtonsInRange());
 
 
@@ -49,9 +51,12 @@ public class PaginatedMenu extends Menu {
      * @return the buttons
      */
     public List<Button> getButtonsInRange() {
-        return this.getButtons().stream()
+        final List<Button> buttons = this.getButtons().stream()
                 .filter(button -> button.getIndex() >= getSize() && button.getIndex() <= getSize() * page)
-                .map(button -> new ButtonBuilder(button).setIndex(button.getIndex() + 9))
-                .collect(Collectors.toList());
+                .map(button -> new ButtonBuilder(button).setIndex(button.getIndex() + 9)).collect(Collectors.toList());
+
+        buttons.addAll(this.paginationButtons);
+
+        return buttons;
     }
 }
