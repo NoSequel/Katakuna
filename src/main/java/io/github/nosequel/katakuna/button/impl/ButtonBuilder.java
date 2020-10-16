942867c37a9a191c48cb04d57d801742a27d03c4
NoSequel <nosequel@protonmail.com>
2020-10-16 05:24:58 +0000

    add fix for pagination indexes

--- src/main/java/io/github/nosequel/katakuna/button/impl/ButtonBuilder.java
+++ src/main/java/io/github/nosequel/katakuna/button/impl/ButtonBuilder.java
@@ -7,7 +7,6 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.Arrays;
